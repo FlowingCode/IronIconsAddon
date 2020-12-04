e9c13dcef740eddbdb7e05178316ef8af7a63f6e
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2020-12-04 11:39:09 -0300

    Update TemplateRendererDemo.java

--- src/test/java/com/flowingcode/vaadin/addons/ironicons/TemplateRendererDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/ironicons/TemplateRendererDemo.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,7 +38,7 @@ public class TemplateRendererDemo extends VerticalLayout {
 		personList.add(new Person(104, "Aaron Atkinson", 18));
 		personList.add(new Person(105, "Jack Woodward", 28));
 		Grid<Person> grid = new Grid<>();
-        grid.setDataProvider(new ListDataProvider<>(personList));
+		grid.setDataProvider(new ListDataProvider<>(personList));
 
 		grid.addColumn(TemplateRenderer.<Person>of(
 				"<iron-icon icon='" + IronIcons.ADD_ALERT.getIconName() + "'></iron-icon>[[item.id]]")
